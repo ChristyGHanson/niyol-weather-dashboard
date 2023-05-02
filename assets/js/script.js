9b2e67aba9b95db7378bb59493cc5b89fd6452be
Christy G. Hanson <34938344+ChristyGHanson@users.noreply.github.com>
2023-05-02 03:01:44 -0600

    Changes to HTML file and README. In the HTML file, I included some buttons for UI functionality. For the README, I updated it with the criteria and the unique title (for now).

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,28 @@
 // Set up fetch request.
-//method
 var cityName = 'Tohatchi'
 // concatenation for Geocoding API
-fetch('api.openweathermap.org/data/2.5/forecast?q=' + cityName + '& appid=70e43b9f8bfe1d2806261c345fcd05fb')
+// must be an http link
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=70e43b9f8bfe1d2806261c345fcd05fb')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
-        // Make two variables for lat and long.
-        var lat = '';
-        var lon = '';
+        console.log(data.city.coord);
+        // Make two variables for latitude (lat) and longitude(lon).
+        // Grab data from lat and lon from the API. 
+        var lat = data.city.coord.lat;
+        var lon = data.city.coord.lon;
         // Use the 5 day/3 hour forecast data
-        // Pass lat and long to the next fetch request next line. 
+        // Pass lat and lon to the next fetch request next line. 
         // units = imperial (avoid Kelvin units)
-        fetch('api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=70e43b9f8bfe1d2806261c345fcd05fb')
+        fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=70e43b9f8bfe1d2806261c345fcd05fb')
             .then(function (response) {
+                // Returns the response. .json makes it readable.
                 return response.json();
             })
             .then(function (data) {
+                // We can console log it because we returned the .json response.
                 console.log(data);
             })
     });
